f618702ee8f9e50f81ae7f75fdd4a357c34c4a05
jenniferliddle <jennifer@jsquared.co.uk>
2012-11-08 17:04:02 +0000

    More refactoring

--- src/shared/parse_bam.h
+++ src/shared/parse_bam.h
@@ -5,15 +5,21 @@
 #define BASE_SOFT_CLIP  (1<<4)
 #define BASE_KNOWN_SNP  (1<<5)
 
-int parse_bam_file_line( samfile_t *fp, bam1_t *bam, int *bam_lane, int *bam_tile, int *bam_x, int *bam_y, int *bam_read, size_t *bam_offset);
+int parse_bam_runinfo(samfile_t *fp, 
+                         bam1_t *bam, 
+                            int *bam_lane, 
+                            int *bam_tile, 
+                            int *bam_x, 
+                            int *bam_y, 
+                            int *bam_read, 
+                         size_t *bam_offset);
 
-int
-parse_bam_file_line_full(
-             samfile_t *fp,
-             bam1_t *bam,
-             char *read_seq,
-             int *read_qual,
-             char *read_ref,
-             int *read_mismatch, const int read_buff_size,
-    HashTable *snp_hash);
+int parse_bam_alignments(samfile_t *fp, 
+                            bam1_t *bam, 
+                              char *read_seq, 
+                               int *read_qual, 
+                              char *read_ref, 
+                               int *read_mismatch, 
+                         const int read_buff_size,
+                         HashTable *snp_hash);
 
